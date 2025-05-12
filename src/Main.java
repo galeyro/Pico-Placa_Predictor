188255ba5125f026e47899b48065b7445cf969af
Galo Guevara <galo.guevara.torres@udla.edu.ec>
2025-05-11 20:56:27 -0500

    fix: update functions in main to manage exceptions when the user enter data

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.sql.SQLOutput;
+import java.time.DateTimeException;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.Scanner;
@@ -24,7 +25,13 @@ public class Main {
         System.out.print("Time: ");
         System.out.println(dateTime.getTime());
 
-        int lastDigit = car.getLastDigit();
+        int lastDigit = -1;
+        try {
+            lastDigit = car.getLastDigit();
+        } catch (Exception e) {
+            System.out.println("Use the format for LICENSE PLATE please ([characters]-[numbers])");
+        }
+
         System.out.print("The last digit of the license plate is: ");
         System.out.println(lastDigit);
 
@@ -34,7 +41,7 @@ public class Main {
         System.out.println("\nIs a restriction day?");
         boolean isRestrictionDay = false;
         isRestrictionDay = restriction.isDayRest(dateTime.getDate(), car.getLastDigit());
-        if (isRestrictionDay){
+        if (isRestrictionDay) {
             System.out.println("Is restriction day!!!");
         } else {
             System.out.println("Free");
@@ -42,8 +49,8 @@ public class Main {
 
         boolean isRestrictionHour = false;
 
-        isRestrictionHour = restriction.isTimeRest(dateTime.getDate(),dateTime.getTime(), car.getLastDigit());
-        if (isRestrictionHour){
+        isRestrictionHour = restriction.isTimeRest(dateTime.getDate(), dateTime.getTime(), car.getLastDigit());
+        if (isRestrictionHour) {
             System.out.println("YOU DO NOT GO OUT");
         } else {
             System.out.println("FREE");
@@ -52,13 +59,15 @@ public class Main {
 
     private static String get_inputLicensePlate() {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the license plate: ");
+        System.out.print("Enter the license plate ([characters]-[numbers]): ");
         return scanner.nextLine();
     }
 
     private static String get_inputDate(){
+
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the date (dd/MM/yyyy): ");
+
         return scanner.nextLine();
     }
 
@@ -68,18 +77,29 @@ public class Main {
         return scanner.nextLine();
     }
 
-    private static LocalDate setDate(String inputDate){
-        int day = parseInt(inputDate.substring(0,2));
-        int month = parseInt(inputDate.substring(3,5));
-        int year = parseInt(inputDate.substring(6,10));
-
+    private static LocalDate setDate(String inputDate) {
+        int year = 0;
+        int month = 0;
+        int day = 0;
+        try {
+            day = parseInt(inputDate.substring(0, 2));
+            month = parseInt(inputDate.substring(3, 5));
+            year = parseInt(inputDate.substring(6, 10));
+        } catch (Exception e) {
+            System.out.println("Use the format for DATE please (dd/HH/yyyy)");
+        }
         return LocalDate.of(year, month, day);
     }
 
-    private static LocalTime setTime(String inputTime){
-        int hour = parseInt(inputTime.substring(0,2));
-        int minute = parseInt(inputTime.substring(3,5));
-
-        return LocalTime.of(hour,minute);
+    private static LocalTime setTime(String inputTime) {
+        int hour = 0;
+        int minute = 0;
+        try {
+            hour = parseInt(inputTime.substring(0, 2));
+            minute = parseInt(inputTime.substring(3, 5));
+        } catch (Exception e) {
+            System.out.println("Use the format for TIME please (hh:mm)");
+        }
+        return LocalTime.of(hour, minute);
     }
 }
